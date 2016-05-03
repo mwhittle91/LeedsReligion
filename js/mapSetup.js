31a9b4d22e27d8ba7db19da5574fcc79031338a9
mwhittle91 <gy14mw@leeds.ac.uk>
2016-05-03 16:16:53 +0100

    finished working code!

--- js/mapSetup.js
+++ js/mapSetup.js
@@ -1,22 +1,11 @@
 var map
 var latlng
-var markers
-var Cmarker
-var Mmarker
-var Smarker
-var Hmarker
-var Jmarker
-var popup
-var Christian
-var Muslim
-var Sikh
-var Jewish
-var hindu
 var geoplaces
 var placefeatures
 var selectedText
 var activityText
-
+var clusters 
+var popUp
 
 var markersAll
 function initmap() {
@@ -27,52 +16,16 @@ function initmap() {
 	map = L.mapbox.map('map', 'mapbox.streets')
 	.setView([53.7998731, -1.5486437], 13);
 
-	// style for the ward geojson
-	var wardStyle = {
-		"color": 'black',
-		"weight": 2,
-		"opacity": 0.9
-	};
-
-	/* add the ward geojson
-	L.geoJson(wards, {
-		style: wardStyle
-	}).addTo(map);
-	*/
-
-	// markes for different religions
-	Christian = L.icon({
-		iconUrl: 'icons/christian.svg',
-		iconSize: [30, 30],
-	})
-	Muslim = L.icon({
-		iconUrl: 'icons/muslim.svg',
-		iconSize: [30, 30],
-	})
-	Sikh = L.icon({
-		iconUrl: 'icons/sikh.svg',
-		iconSize: [30, 30],
-	})
-	Jewish = L.icon({
-		iconUrl: 'icons/jewish.svg',
-		iconSize: [30, 30],
-	})
-	Hindu = L.icon({
-		iconUrl: 'icons/hindu.svg',
-		iconSize: [30, 30],
-	})
-
 	// get the geojson into an array to make querying easier
 	geoplaces = places
 	placefeatures = []
 	placefeatures = geoplaces.features
 	//Set up markerclusterer
-	markersAll = new L.MarkerClusterGroup()
-	markers = new L.MarkerClusterGroup()
 	// Set up arrays for religion dropdown
 	var religionArray = []
 	var uniqueReligion
-
+	
+	clusters = new L.MarkerClusterGroup();
 
 	// for loop to search through data
 	for (ID in placefeatures) {
@@ -82,40 +35,6 @@ function initmap() {
 		uniqueReligion = religionArray.filter(function(elem, pos) {
 			return religionArray.indexOf(elem) == pos;
 		});
-
-
-		popup = "<H3>" + placefeatures[ID].properties.Organisation + "</H3>"
-				+ "<BR>" + "<p>" + "<b>Religion: </b>" + placefeatures[ID].properties.Religion
-				+ "<BR>" + "<b>Address: </b>" + placefeatures[ID].properties.Address_Line_1
-					+ "<BR>" + placefeatures[ID].properties.Area
-					+ "<BR>" + placefeatures[ID].properties.City
-					+ "<BR>" +  placefeatures[ID].properties.Postcode
-				+ "<BR>"+ "<b>Telephone: </b>" +  placefeatures[ID].properties.Telephone
-				+ "<BR>"+ "<b>Website: </b>" +  '<a href="http://"'+placefeatures[ID].properties.Website+ '">' +placefeatures[ID].properties.Website+ '</a>'
-				+ "<BR>"+ "<b>Activities: </b>" +  placefeatures[ID].properties.Activities
-
-
-
-		// create the location for the marker
-		latlng = [placefeatures[ID].geometry.coordinates[1],placefeatures[ID].geometry.coordinates[0]]
-		// create markers and add them to the clusterer
-		if  (placefeatures[ID].properties.Religion === 'Christian') {
-			Cmarker  = new L.Marker(latlng, {icon: Christian}).bindPopup(popup);
-			markersAll.addLayer(Cmarker);
-		} else if (placefeatures[ID].properties.Religion === 'Muslim')  {
-			Mmarker = new L.Marker(latlng, {icon: Muslim}).bindPopup(popup);
-			markersAll.addLayer(Mmarker);
-		} else if (placefeatures[ID].properties.Religion === 'Sikh')  {
-			Smarker = new L.Marker(latlng, {icon: Sikh}).bindPopup(popup);
-			markersAll.addLayer(Smarker);
-		} else if (placefeatures[ID].properties.Religion === 'Hindu')  {
-			Hmarker = new L.Marker(latlng, {icon: Sikh}).bindPopup(popup);
-			markersAll.addLayer(Hmarker);
-		}else if (placefeatures[ID].properties.Religion === 'Jewish')  {
-			Jmarker = new L.Marker(latlng, {icon: Jewish}).bindPopup(popup);
-			markersAll.addLayer(Jmarker);
-		}
-		map.addLayer(markersAll);
 	}
 
 	$.each(uniqueReligion, function(val, text) {
@@ -124,76 +43,229 @@ function initmap() {
 		);
 	});
 
-	$("#email").hide();
-
-	$(document).ready(
-    function(){
-        $("#button").click(function () {
-            $("#email").show("fast");
-        });
-    });
 }
 
-function religionDrop() {
+function select() { 
 
-		geoplaces = places
-		placefeatures = []
-		placefeatures = geoplaces.features
-		map.removeLayer(markersAll);
-		map.removeLayer(markers);
-		markers = new L.MarkerClusterGroup()
-		// for loop to search through data
-		for (ID in placefeatures) {
+	var relDrop =  document.getElementById("religion-select");
+	var relValue = relDrop.options[relDrop.selectedIndex].value;
+	
+	var actDrop =  document.getElementById("activity-select");
+	var actValue = actDrop.options[actDrop.selectedIndex].value;
 
-		skillsSelect = document.getElementById("religion-select");
-		selectedText = skillsSelect.options[skillsSelect.selectedIndex].text
-		latlng = [placefeatures[ID].geometry.coordinates[1],placefeatures[ID].geometry.coordinates[0]]
+	map.removeLayer(clusters);
+	clusters = new L.MarkerClusterGroup();
+	
+	
+	for (ID in placefeatures) {
+		
+		popUp = "<H3>" + placefeatures[ID].properties.Organisation + "</H3>"
+				+ "<BR>" + "<p>" + "<b>Religion: </b>" + placefeatures[ID].properties.Religion
+				+ "<BR>" + "<b>Address: </b>" + placefeatures[ID].properties.Address_Line_1
+				+ "<BR>" + placefeatures[ID].properties.Area
+				+ "<BR>" + placefeatures[ID].properties.City
+				+ "<BR>" +  placefeatures[ID].properties.Postcode
+				+ "<BR>"+ "<b>Telephone: </b>" +  placefeatures[ID].properties.Telephone
+				+ "<BR>"+ "<b>Website: </b>" +  '<a href="http://"'+placefeatures[ID].properties.Website+ '">' +placefeatures[ID].properties.Website+ '</a>'
+				+ "<BR>"+ "<b>Activities: </b>" +  placefeatures[ID].properties.Activities
+		
+		if (relValue === "" && actValue === ""){
+			plotPlaces()
 
+		} else if (relValue === placefeatures[ID].properties.Religion && actValue === "") {
+			plotPlaces()
+		}else if (relValue === placefeatures[ID].properties.Religion && actValue === "Education/Languages") {
+			plotEducation()
+		}else if (relValue === placefeatures[ID].properties.Religion && actValue === "Outreach/Counselling") {
+			plotOutreach()
+		}else if (relValue === placefeatures[ID].properties.Religion && actValue === "Lunch Club") {
+			plotLunchClub()
+		}else if (relValue === placefeatures[ID].properties.Religion && actValue === "Youth Club") {
+			plotYouthClub()
+		}else if (relValue === placefeatures[ID].properties.Religion && actValue === "Fitness") {
+			plotFitness()
+		}else if (relValue === "" && actValue === "Education/Languages") {
+			plotEducation()
+		}else if (relValue === "" && actValue === "Outreach/Counselling") {
+			plotOutreach()
+		}else if (relValue === "" && actValue === "Lunch Club") {
+			plotLunchClub()
+		}else if (relValue === "" && actValue === "Youth Club") {
+			plotYouthClub()
+		}else if (relValue === "" && actValue === "Fitness") {
+			plotFitness()
+		}
 
+	function plotEducation() { 
+		
+			if (placefeatures[ID].properties.Religion === 'Christian' && placefeatures[ID].properties.Education=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Muslim' && placefeatures[ID].properties.Education=== "Yes") {
+			plotPlaces()
+			}	
+			else if (placefeatures[ID].properties.Religion === 'Sikh' && placefeatures[ID].properties.Education=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Hindu' && placefeatures[ID].properties.Education=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Jewish' && placefeatures[ID].properties.Education=== "Yes") {
+			plotPlaces()
+			}
+			
+		}
+	
+	function plotOutreach() { 
+		
+			if (placefeatures[ID].properties.Religion === 'Christian' && placefeatures[ID].properties.Outreach=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Muslim' && placefeatures[ID].properties.Outreach=== "Yes") {
+			plotPlaces()
+			}	
+			else if (placefeatures[ID].properties.Religion === 'Sikh' && placefeatures[ID].properties.Outreach=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Hindu' && placefeatures[ID].properties.Outreach=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Jewish' && placefeatures[ID].properties.Outreach=== "Yes") {
+			plotPlaces()
+			}
+		}
+	
+		function plotLunchClub() { 
+		
+			if (placefeatures[ID].properties.Religion === 'Christian' && placefeatures[ID].properties.LunchClub=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Muslim' && placefeatures[ID].properties.LunchClub=== "Yes") {
+			plotPlaces()
+			}	
+			else if (placefeatures[ID].properties.Religion === 'Sikh' && placefeatures[ID].properties.LunchClub=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Hindu' && placefeatures[ID].properties.LunchClub=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Jewish' && placefeatures[ID].properties.LunchClub=== "Yes") {
+			plotPlaces()
+			}
+		}
+		
+		function plotYouthClub() { 
+		
+			if (placefeatures[ID].properties.Religion === 'Christian' && placefeatures[ID].properties.YouthClub=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Muslim' && placefeatures[ID].properties.YouthClub=== "Yes") {
+			plotPlaces()
+			}	
+			else if (placefeatures[ID].properties.Religion === 'Sikh' && placefeatures[ID].properties.YouthClub=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Hindu' && placefeatures[ID].properties.YouthClub=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Jewish' && placefeatures[ID].properties.YouthClub=== "Yes") {
+			plotPlaces()
+			}
+		}
+		
+		function plotFitness() { 
+		
+			if (placefeatures[ID].properties.Religion === 'Christian' && placefeatures[ID].properties.Fitness=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Muslim' && placefeatures[ID].properties.Fitness=== "Yes") {
+			plotPlaces()
+			}	
+			else if (placefeatures[ID].properties.Religion === 'Sikh' && placefeatures[ID].properties.Fitness=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Hindu' && placefeatures[ID].properties.Fitness=== "Yes") {
+			plotPlaces()
+			}
+			else if (placefeatures[ID].properties.Religion === 'Jewish' && placefeatures[ID].properties.Fitness=== "Yes") {
+			plotPlaces()
+			}
+		}
+	
+	}	
+		function plotPlaces() { 
+		
+		console.log("helloworld")
 
-				if (selectedText === 'Select Religion' ) {
-
-					map.removeLayer(markers);
-					map.addLayer(markersAll);
-
-			}else if (selectedText === 'Christian' && placefeatures[ID].properties.Religion === 'Christian') {
-
-					Cmarker  = new L.Marker(latlng, {icon: Christian}).bindPopup(popup);
-					markers.addLayer(Cmarker);
-			}else if (selectedText === 'Muslim' && placefeatures[ID].properties.Religion === 'Muslim') {
-
-					Mmarker = new L.Marker(latlng, {icon: Muslim}).bindPopup(popup);
-					markers.addLayer(Mmarker);
-			}else if (selectedText === 'Sikh' && placefeatures[ID].properties.Religion === 'Sikh') {
-
-					Smarker = new L.Marker(latlng, {icon: Sikh}).bindPopup(popup);
-					markers.addLayer(Smarker);
-			}else if (selectedText === 'Hindu' && placefeatures[ID].properties.Religion === 'Hindu') {
-
-					Hmarker = new L.Marker(latlng, {icon: Hindu}).bindPopup(popup);
-					markers.addLayer(Hmarker);
-			}else if (selectedText === 'Jewish' && placefeatures[ID].properties.Religion === 'Jewish') {
-					Jmarker = new L.Marker(latlng, {icon: Jewish}).bindPopup(popup);
-					markers.addLayer(Jmarker);
-			}map.addLayer(markers);
+			latlng = [placefeatures[ID].geometry.coordinates[1],placefeatures[ID].geometry.coordinates[0]]
+			
+			if (placefeatures[ID].properties.Religion === 'Christian') {
+			
+				var christianIcon = L.icon({
+				iconUrl: 'icons/christian.svg',
+				iconSize: [30, 30],
+				})
+				marker = new L.Marker(latlng, { 
+					icon: christianIcon
+				})
+				addMarker()
+			}	
+			
+			else if (placefeatures[ID].properties.Religion === 'Muslim') {
+			
+				var muslimIcon = L.icon({
+				iconUrl: 'icons/muslim.svg',
+				iconSize: [30, 30],
+				})
+				marker = new L.Marker(latlng, { 
+					icon: muslimIcon
+				})
+				addMarker()
+			}	
+			
+			else if (placefeatures[ID].properties.Religion === 'Sikh') {
+			
+				var sikhIcon = L.icon({
+				iconUrl: 'icons/sikh.svg',
+				iconSize: [30, 30],
+				})
+				marker = new L.Marker(latlng, { 
+					icon: sikhIcon
+				})
+				addMarker()
+			}
+			
+			else if (placefeatures[ID].properties.Religion === 'Hindu') {
+			
+				var hinduIcon = L.icon({
+				iconUrl: 'icons/hindu.svg',
+				iconSize: [30, 30],
+				})
+				marker = new L.Marker(latlng, { 
+					icon: hinduIcon
+				})
+				addMarker()
+			}
+			
+			else if (placefeatures[ID].properties.Religion === 'Jewish') {
+			
+				var jewishIcon = L.icon({
+				iconUrl: 'icons/jewish.svg',
+				iconSize: [30, 30],
+				})
+				marker = new L.Marker(latlng, { 
+					icon: jewishIcon
+				})
+				addMarker()
+			}
+		
 		}
 	}
 
-function activityDrop() {
+function addMarker() {
 
-	geoplaces = places
-	placefeatures = []
-	placefeatures = geoplaces.features
-
-	for (ID in placefeatures) {
-
-	activitySelect = document.getElementById("activity-select");
-	activityText = activitySelect.options[activitySelect.selectedIndex].text
-	skillsSelect = document.getElementById("religion-select");
-	selectedText = skillsSelect.options[skillsSelect.selectedIndex].text
-	latlng = [placefeatures[ID].geometry.coordinates[1],placefeatures[ID].geometry.coordinates[0]]
-
-	}
-	console.log(activityText, selectedText)
-
+	clusters.addLayer(marker)
+	map.addLayer(clusters);
+	marker.bindPopup(popUp);
 }
+
